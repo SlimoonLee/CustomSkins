8b4c6e096ff8e6be60f8bd8e611a2646f2d3a636
moon sli <752831426@qq.com>
2021-04-16 11:25:09 +0800

    添加项目文件。

---
+++ CustomSkins/ChangeSkin.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+using UnityModManagerNet;
+using BepInEx;
+using AI;
+using GameData;
+using System.Reflection.Emit;
+using System.IO;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using YanLib;
+
+namespace CustomSkins
+{
+	public class TurnToSkin
+	{
+		//点击事件
+		class ClickAction : MonoBehaviour, IPointerClickHandler
+		{
+			int _clothId;
+			public void OnPointerClick(PointerEventData eventData)
+			{
+				Button okbtn = YesOrNoWindow.instance.yesOrNoWindow.Find("YesButton").GetComponent<Button>();
+				okbtn.onClick.AddListener(ClickYes);
+				Button nobtn = YesOrNoWindow.instance.yesOrNoWindow.Find("NoButton").GetComponent<Button>();
+				nobtn.onClick.AddListener(ClickNo);
+				YesOrNoWindow.instance.windowMask.SetActive(true);
+				YesOrNoWindow.instance.SetYesOrNoWindow(1997052201, "改变形象", "是否将本衣着转化为一个拥有随机特殊立绘的皮肤（不影响属性）？", true, true);
+			}
+			public void SetParam(int clothId)
+			{
+				_clothId = clothId;
+			}
+			private void ClickYes()//在这里做正事
+			{
+				int id = OnClick.instance.ID;
+				if (id == 1997052201)
+				{
+					TurnToSpecialSkin(_clothId);
+					//YanLib.DataManipulator.Actor.DumpActorData(DateFile.instance.MianActorID(), false, false, @"C:\Users\Slimoon\Desktop\MainActor");
+				/*	Dictionary<int, Dictionary<int, string>> itempower = DateFile.instance.itemPowerDate;
+					foreach (KeyValuePair<int, Dictionary<int, string>> power in itempower)
+					{
+						foreach (KeyValuePair<int, string> pow in power.Value)
+						{
+							Console.WriteLine("Key1={0}, Key2={1}, Content={2}", power.Key, pow.Key, pow.Value);
+						}
+					}*/
+				}
+				RemoveBind();
+			}
+
+			private void ClickNo()
+			{
+				RemoveBind();
+			}
+			private static void RemoveBind()
+			{
+				Button okbtn = YesOrNoWindow.instance.yesOrNoWindow.Find("YesButton").GetComponent<Button>();
+				okbtn.onClick.RemoveAllListeners();
+				Button nobtn = YesOrNoWindow.instance.yesOrNoWindow.Find("NoButton").GetComponent<Button>();
+				nobtn.onClick.RemoveAllListeners();
+			}
+		}
+		//预先设置点击事件
+		[HarmonyPatch(typeof(SetItem), "SetActorMenuItemIcon")]
+		public static class EasyRefine_SetActorMenuItemIcon_Patch
+		{
+			static void Postfix(SetItem __instance, int itemId)
+			{
+				AddClickAction(__instance.gameObject, itemId);
+			}
+		}
+		[HarmonyPatch(typeof(SetItem), "SetActorEquipIcon")]
+		public static class EasyRefine_SetActorEquipIcon_Patch
+		{
+			static void Postfix(SetItem __instance, int itemId)
+			{
+				AddClickAction(__instance.gameObject, itemId);
+			}
+		}
+
+		[HarmonyPatch(typeof(DateFile), "SetItemName")]//显示物品当前ID和类别ID，测试用
+		public static class SetItemName_Patch
+		{
+			public static void Postfix(ref string __result, int id)
+			{
+				if(int.Parse(DateFile.instance.GetItemDate(id, 15, true)) != 0 && int.Parse(DateFile.instance.GetItemDate(id, 34, true)) != 0)
+				__result = __result + "\n立绘ID："+ DateFile.instance.GetItemDate(id, 34, true);
+
+			}
+
+		}
+		static void AddClickAction(GameObject icon, int itemId)
+		{
+			//判断是不是衣着
+			bool flag1 = int.Parse(DateFile.instance.GetItemDate(itemId, 15, true)) ==0;
+			if (flag1)
+			{
+				GameObject.Destroy(icon.GetComponent<ClickAction>());
+				return;
+			}
+		/*	else
+			{
+				bool flag2 = Items.GetItemProperty(itemId, 901).Equals(Items.GetItemProperty(itemId, 902));
+				if (flag2)
+					return;
+			}*/
+
+			//添加相应处理Component,注入参数
+			var clickActions = icon.GetComponents<ClickAction>();
+			if (clickActions.Length >= 1)//避免重复添加
+			{
+				clickActions[0].SetParam(itemId);
+			}
+			else
+			{
+				var actionstub = icon.AddComponent<ClickAction>();
+				actionstub.SetParam(itemId);
+			}
+		}
+
+		public static void TurnToSpecialSkin(int _clothId)
+		{
+			string appPath = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			appPath = Path.Combine(appPath, "big");
+			//List<string> SkinsBig = null;
+			var SkinsBig = Directory.GetFiles(appPath, "*.png");
+			int len = SkinsBig.Count();
+			for (int i = 0; i < len; i++)
+			{
+				SkinsBig[i] = SkinsBig[i].Replace(appPath + @"\", "");
+				SkinsBig[i] = SkinsBig[i].Replace(".png", "");
+				//Debug.Log(SkinsBig[i]);
+			}
+			if (len > 0)
+			{
+				string randSkin = SkinsBig[UnityEngine.Random.Range(0, len)];
+				//Items.SetItemProperty(_clothId, 902, randSkin);//衣物的property只有三个，901耐久，902耐久上限，999类别id
+				//Items.SetItemProperty(_clothId, 901, randSkin);
+				//if (int.Parse(DateFile.instance.GetItemDate(_clothId, 34)) == 0)
+					DateFile.instance.ChangItemDate(_clothId, 34, int.Parse(randSkin) * 10, true);
+				//else
+					//DateFile.instance.ChangItemDate(_clothId, 34, 10, false);//茄子真有够闲
+				//DateFile.instance.ChangItemDate(_clothId, 504, 407 * 10, true);
+				//Debug.Log(DateFile.instance.GetItemDate(_clothId, 504, true, -1));
+			}
+		}
+	}
+}
---
+++ CustomSkins/Main.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+using UnityModManagerNet;
+using BepInEx;
+using AI;
+using GameData;
+using System.Linq;
+using System.Reflection.Emit;
+using System.IO;
+
+namespace CustomSkins
+{
+    [BepInPlugin("Slimoon.CustomSkins", "CustomSkins", "1.0.0")]
+    public class Main : BaseUnityPlugin
+    {
+        void Start()
+        {
+            new Harmony("Slimoon.CustomSkins").PatchAll();
+            //    LoadSprite("a_s.png");
+            //    LoadSprite("a.png");
+        }
+
+        public static Sprite LoadSprite(string imageName, bool small)
+        {
+            string appPath = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			if (small)
+			{
+                appPath = Path.Combine(appPath, "small");
+			}
+			else
+			{
+                appPath = Path.Combine(appPath, "big");
+            }
+            string path = Path.Combine(appPath, imageName);
+            if (!File.Exists(path))
+            {
+            //    Console.WriteLine($"图片路径[{path}]不存在");
+                return null;
+            }
+            var fileData = File.ReadAllBytes(path);
+            var texture = new Texture2D(2, 2);
+            texture.LoadImage(fileData);
+            //Console.WriteLine($"成功加载图片[{path}]\n大小为{texture.width}*{texture.height}");
+            return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0, 0), 100);
+        }
+
+        [HarmonyPatch(typeof(ActorFace), "UpdateFace", new Type[]{typeof(int), typeof(int), typeof(int), typeof(int), typeof(int[]), typeof(int[]), typeof(int), typeof(bool), typeof(bool)})]
+        public static class UpdateFace_Patch
+        {
+            public static bool Prefix(int actorId, ActorFace __instance)
+			{
+                int SpecialclotheIndex = 0;
+                int itemid = int.Parse(DateFile.instance.GetActorDate(actorId, 305, true));
+                //Debug.Log(itemid.ToString());//进入游戏的主界面也会用到这个函数，此时游戏数据未加载所以itemid为0，淦哦，用这一点来屏蔽主界面时候的补丁运行
+                if (itemid != 0)
+                {
+                    SpecialclotheIndex = int.Parse(DateFile.instance.GetItemDate(itemid, 34, true, -1));//902
+                    if (SpecialclotheIndex>100)
+
+                    {
+                        __instance.ageImage.gameObject.SetActive(false);
+                        __instance.nose.gameObject.SetActive(false);
+                        __instance.faceOther.gameObject.SetActive(false);
+                        __instance.eye.gameObject.SetActive(false);
+                        __instance.eyePupil.gameObject.SetActive(false);
+                        __instance.eyebrows.gameObject.SetActive(false);
+                        __instance.mouth.gameObject.SetActive(false);
+                        __instance.beard.gameObject.SetActive(false);
+                        __instance.hair1.gameObject.SetActive(false);
+                        __instance.hair2.gameObject.SetActive(false);
+                        __instance.hairOther.gameObject.SetActive(false);
+                        __instance.clothes.gameObject.SetActive(false);
+                        __instance.clothesColor.gameObject.SetActive(false);
+                        __instance.body.gameObject.SetActive(true);
+                        bool flag2 = __instance.smallSize;
+                        if (flag2)
+                        {
+                            var temp = LoadSprite(SpecialclotheIndex.ToString() + ".png", true);
+                            if (temp == null) temp = LoadSprite(SpecialclotheIndex.ToString() + ".png", false);
+                            if (temp == null) return true;
+                            __instance.body.sprite = temp;
+                        }
+                        else
+                        {
+                            var temp = LoadSprite(SpecialclotheIndex.ToString() + ".png", false);
+                            if (temp == null) return true;
+                            __instance.body.sprite = temp;
+                        }
+                        __instance.body.color = new Color(1f, 1f, 1f, 1f);
+                        //int id = DateFile.instance.MianActorID();
+                        //int itemid = int.Parse(DateFile.instance.GetActorDate(id, 305, true));
+                        //Debug.Log(@"it's id is "+itemid.ToString());//+" "+DateFile.instance.GetItemDate(itemid, 999, true, -1)
+                        return false;
+                    }
+                    else return true;
+                }
+                else return true;
+                
+                
+            }
+		}
+    }
+}
+            //获取包含当前执行的代码的程序集的加载文件的完整路径，本mod就用这个了
+            //测试位置：D:\SteamLibrary\steamapps\common\The Scroll Of Taiwu\BepInEx\plugins\CustomSkins
+            //var appPath = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            //Console.WriteLine(appPath);
+
+            //获取模块的完整路径
+            //string path = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+            //Console.WriteLine(path);
+
+            //获取和设置当前目录(该进程从中启动的目录)的完全限定目录。
+            //var dicPath = System.Environment.CurrentDirectory;
+            //Console.WriteLine(dicPath);
+
+            //获取程序的基目录
+            //string basePath = System.AppDomain.CurrentDomain.BaseDirectory;
+            //Console.WriteLine(basePath);
+
+            //获取和设置包括该应用程序的目录的名称
+            //string domainPath = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            //Console.WriteLine(domainPath);
